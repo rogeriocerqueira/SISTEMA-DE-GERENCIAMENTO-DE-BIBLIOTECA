b0095e403506635cc244327c3871ced47453a14f
João Macedo <joao.macedo.lima@hotmail.com>
2023-09-04 19:27:52 -0300

    :sparkles: feat: Add "Person" abstract class

---
+++ src/Model/Person.java
@@ -0,0 +1,70 @@
+package Model;
+
+public abstract class  Person {
+
+    private String id;
+    private String cpf;
+    private String name;
+    private String office;
+    private int age;
+    private String nPhone;
+    private String email;
+
+    /*SETS E GETS*/
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOffice() {
+        return office;
+    }
+
+    public void setOffice(String office) {
+        this.office = office;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getnPhone() {
+        return nPhone;
+    }
+
+    public void setnPhone(String nPhone) {
+        this.nPhone = nPhone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
