77fe45eddd8492cd0181757bc5b8a81b989ece10
João Macedo <joao.macedo.lima@hotmail.com>
2023-09-04 19:24:51 -0300

    :sparkles: feat: Add "Book" class

---
+++ src/Model/Book.java
@@ -0,0 +1,70 @@
+package Model;
+
+public class Book {
+
+    private String isbn;
+    private String title;
+    private String author;
+    private String publisher;
+    private String year;
+    private String category;
+    private int number;
+
+    /*SETS E GETS*/
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
