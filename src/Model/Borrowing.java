2c2cd334578176519b4c0580c88445b95c961440
João Macedo <joao.macedo.lima@hotmail.com>
2023-09-04 19:25:51 -0300

    :sparkles: feat: Add "Borrowing" class

---
+++ src/Model/Borrowing.java
@@ -0,0 +1,15 @@
+package Model;
+
+public class Borrowing {
+
+    private String idB;
+    private String idUser;
+    private String userName;
+    private String isbn;
+    private String bookName;
+    /*private Date dStart;*/
+    /*private Date devolution;*/
+    /*private Date dEnd;*/
+    private boolean rSituation;
+
+}
