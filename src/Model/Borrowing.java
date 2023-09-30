dc7a6d9581f5ec658a448329f7b12dd97ddef81f
João Macedo <joao.macedo.lima@hotmail.com>
2023-09-30 15:57:47 -0300

    :sparkles: feat: Library "Date" added and methods SET and GET.

--- src/Model/Borrowing.java
+++ src/Model/Borrowing.java
@@ -1,5 +1,7 @@
 package Model;
 
+import java.util.Date;
+
 public class Borrowing {
 
     private String idB;
@@ -7,9 +9,80 @@ public class Borrowing {
     private String userName;
     private String isbn;
     private String bookName;
-    /*private Date dStart;*/
-    /*private Date devolution;*/
-    /*private Date dEnd;*/
+    private Date dStart;
+    private Date devolution;
+    private Date dEnd;
     private boolean rSituation;
 
+    public String getIdB() {
+        return idB;
+    }
+
+    public void setIdB(String idB) {
+        this.idB = idB;
+    }
+
+    public String getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(String idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public Date getdStart() {
+        return dStart;
+    }
+
+    public void setdStart(Date dStart) {
+        this.dStart = dStart;
+    }
+
+    public Date getDevolution() {
+        return devolution;
+    }
+
+    public void setDevolution(Date devolution) {
+        this.devolution = devolution;
+    }
+
+    public Date getdEnd() {
+        return dEnd;
+    }
+
+    public void setdEnd(Date dEnd) {
+        this.dEnd = dEnd;
+    }
+
+    public boolean isrSituation() {
+        return rSituation;
+    }
+
+    public void setrSituation(boolean rSituation) {
+        this.rSituation = rSituation;
+    }
 }
