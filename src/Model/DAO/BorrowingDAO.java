1882997baf073652e1ad39363bcdd2d7a8dd653e
João Macedo <joao.macedo.lima@hotmail.com>
2023-10-01 18:03:53 -0300

    :sparkles: feat: Change of package of related class.

--- src/Model/DAO/BorrowingDAO.java
+++ src/Model/DAO/BorrowingDAO.java
@@ -1,12 +1,13 @@
 package Model.DAO;
 
+import Controller.ControllerBook;
 import Model.Borrowing;
 
 import java.util.Date;
 
 public interface BorrowingDAO {
 
-    public void create (String idB, String idUser, String userName, String isbn, String bookName, Date dStart, boolean rSituation, BookDAOImp BookImp);
+    public void create (String idB, String idUser, String userName, String isbn, String bookName, Date dStart, boolean rSituation, ControllerBook BookImp);
     public Borrowing read(String idB, String whichList);
     public void update(String idB, String nIdUser, String nUserName, String nIsbn, String nBookName);
     public void delete(String idB);
