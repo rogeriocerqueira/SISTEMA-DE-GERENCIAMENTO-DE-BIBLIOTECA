dcc9a0d40033fd58464a5646d22aecede2efa41f
Rogério Cerqueira <rogeriocerqueira.uefs@gmail.com>
2023-10-01 16:01:21 -0300

    :sparkles: feat: Interface "Administrator" added.

---
+++ src/Model/DAO/AdministratorDAO.java
@@ -0,0 +1,12 @@
+package Model.DAO;
+import Model.Administrator;
+
+import java.util.ArrayList;
+
+public interface AdministratorDAO {
+    public void create(String name, String cpf, String office, int age, String nPhone, String email);
+    public ArrayList<Administrator> read(String cpf);
+    public void update(String name, String cpf, String office, int age, String nPhone, String email);
+    public void delete(int id);
+
+}
