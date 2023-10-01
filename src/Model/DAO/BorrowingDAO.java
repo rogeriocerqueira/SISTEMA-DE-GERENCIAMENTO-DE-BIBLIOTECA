08a4d77d4f9ae2388f26916d6063f16d5f25cd66
João Macedo <joao.macedo.lima@hotmail.com>
2023-10-01 18:28:10 -0300

    :sparkles: feat: Change of attributes.

--- src/Model/DAO/BorrowingDAO.java
+++ src/Model/DAO/BorrowingDAO.java
@@ -1,13 +1,14 @@
 package Model.DAO;
 
 import Controller.ControllerBook;
+import Model.Book;
 import Model.Borrowing;
 
 import java.util.Date;
 
 public interface BorrowingDAO {
 
-    public void create (String idB, String idUser, String userName, String isbn, String bookName, Date dStart, boolean rSituation, ControllerBook BookImp);
+    public void create (Borrowing aBorrowing, ControllerBook BookImp);
     public Borrowing read(String idB, String whichList);
     public void update(String idB, String nIdUser, String nUserName, String nIsbn, String nBookName);
     public void delete(String idB);
