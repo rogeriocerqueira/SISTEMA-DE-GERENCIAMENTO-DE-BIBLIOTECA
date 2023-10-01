2fb4df22dd9d87ecfe37e47ef805865aac770ea0
João Macedo <joao.macedo.lima@hotmail.com>
2023-10-01 18:27:47 -0300

    :sparkles: feat: Change of attributes.

--- src/Model/DAO/BookDAO.java
+++ src/Model/DAO/BookDAO.java
@@ -6,9 +6,9 @@ import java.util.ArrayList;
 
 public interface BookDAO {
 
-    public void create(String idBook, String isbn, String title, String author, String publisher, String year, String category, int number);
+    public void create(Book aBook);
     public ArrayList<Book> read(String isbn, String title, String author, String category);
-    public void update(String idBook,String nIsbn, String nTitle, String nAuthor, String nPublisher, String nYear, String nCategory, int nNumber);
+    public void update(String idBook, Book aBook);
     public void delete(String idBook);
 
 }
