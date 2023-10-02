a22f8d105b9056abd705ed0387132f4358eca326
João Macedo <joao.macedo.lima@hotmail.com>
2023-10-02 13:36:13 -0300

    :sparkles: feat: Exclusion of library and correction name of Interface.

--- src/Model/DAO/BorrowingDAO.java
+++ src/Model/DAO/BorrowingDAO.java
@@ -1,14 +1,11 @@
 package Model.DAO;
 
-import Controller.ControllerBook;
-import Model.Book;
+import Controller.BookController;
 import Model.Borrowing;
-
-import java.util.Date;
 
 public interface BorrowingDAO {
 
-    public void create (Borrowing aBorrowing, ControllerBook BookImp);
+    public void create (Borrowing aBorrowing, BookController BookImp);
     public Borrowing read(String idB, String whichList);
     public void update(String idB, String nIdUser, String nUserName, String nIsbn, String nBookName);
     public void delete(String idB);
