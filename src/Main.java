f56b0a72e9612dc9a4fdaf99069f7ea261c11258
João Macedo <joao.macedo.lima@hotmail.com>
2023-10-02 23:20:15 -0300

    :bulb: docs: Anti-plagiarism statement.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+/*Autor: João Victor Macedo dos Santos Lima e Rogério dos Santos Cerqueira
+        Componente Curricular: Programação
+        Concluido em: 02/10/2023
+        Declaro que este código foi elaborado por mim de forma individual e não contém nenhum
+        trecho de código de outro colega ou de outro autor, tais como provindos de livros e
+        apostilas, e páginas ou documentos eletrônicos da Internet. Qualquer trecho de código
+        de outra autoria que não a minha está destacado com uma citação para o autor e a fonte
+        do código, e estou ciente que estes trechos não serão considerados para fins de avaliação.*/
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
